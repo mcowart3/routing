3d261f68df54863738f6dbd3f89ae1f0d9bb34fb
Cowart, Micaiah <mcowart3@gatech.edu>
2021-06-07 13:27:36 -0400

    Initial commit

---
+++ graph.py
@@ -0,0 +1,3 @@
+import networkx as nx
+fti = nx.Graph()
+fti.add_nodes_from(range(1, 22))
---
+++ node.py
@@ -0,0 +1,6 @@
+class node:
+    def __init__(self, name, lat, long):
+        self.name = name
+        self.lat = lat
+        self.long = long
+        
